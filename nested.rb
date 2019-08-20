2a3b072253159751dec1242b18d582a0a96cb18a
Jonathan Tan <jonathantan@brandeis.edu>
2019-08-20 18:06:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -20,8 +20,7 @@ def hopper
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
+
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -39,6 +38,7 @@ def alan_kay_is_known_for
      }
      programmer_hash[:alan_kay][:known_for]
 end
+
 
 def dennis_ritchies_language
 	programmer_hash = 
